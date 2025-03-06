e8ffea2536edadad54bf47f219140ba77b449215
Santosh Pawar <santoshjpawar@gmail.com>
2025-03-06 15:44:25 -0500

    Update test.ps1

--- scripts/test.ps1
+++ scripts/test.ps1
@@ -15,7 +15,7 @@ param(
 
 $authenticationToken = [System.Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$accessToken"))
 $headers = @{
-        "Authorization" = [String]::Format("Bearer {0}", $authenticationToken)
+        "Authorization" = [String]::Format("Basic {0}", $authenticationToken)
         "Content-Type"  = "application/json"
 }
 
