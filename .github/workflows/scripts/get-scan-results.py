1c104849cb99107dafca32cb0ef59120d0cdf15e
Santosh Pawar <santoshjpawar@gmail.com>
2025-03-13 18:28:02 -0400

    Update get-scan-results.py

--- .github/workflows/scripts/get-scan-results.py
+++ .github/workflows/scripts/get-scan-results.py
@@ -21,6 +21,9 @@ for issue in issues:
     if issue.get('status') == "CLOSED":
         continue
     f = open("prompt.txt", "a")
+    with open("action-prompt.txt", 'r') as file:
+        content = file.read()
+        f.write(content + "\n")
     f.write("Vulnerability Details\n")
     f.write("```\n")
     key = issue.get('key')
