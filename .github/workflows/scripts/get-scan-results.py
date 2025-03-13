becd673c7142f0c51ac291358215289cbb00c6e6
Santosh Pawar <santoshjpawar@gmail.com>
2025-03-13 18:13:54 -0400

    Update get-scan-results.py

--- .github/workflows/scripts/get-scan-results.py
+++ .github/workflows/scripts/get-scan-results.py
@@ -13,44 +13,44 @@ params = {
     'projects': 'sp-org-1_repo-1',
 }
 response = requests.get('https://sonarcloud.io/api/issues/search', params=params, headers=headers, auth=HTTPBasicAuth(username, password))
-#print(response)
-#print(response.text)
-
 
 # Parse the JSON response
 jsonout = json.loads(response.text)
-#print(jsonout)
-#print("====")
-#print(json.dumps(jsonout))
 issues = jsonout.get('issues', [])
 for issue in issues:
     if issue.get('status') == "CLOSED":
         continue
-    print("------")
+    f = open("prompt.txt", "a")
+    f.write("Vulnerability Details")
+    f.write("```")
     key = issue.get('key')
-    print("key:", key)
+    f.write("key:", key)
     rule = issue.get('rule')
-    print("rule:", rule)
+    f.write("rule:", rule)
     message = issue.get('message')
-    print("message:", message)
-    filename = issue.get('component').split(":")[1]
-    print("filename:", filename)
+    f.write("message:", message)
     if(issue.get('textRange') == None):
         continue
     startline = issue.get('textRange').get('startLine')
-    print("startline:", startline)
+    f.write("startline:", startline)
     endline = issue.get('textRange').get('endLine')  
-    print("endline:", endline)
+    f.write("endline:", endline)
     startoffset = issue.get('textRange').get('startOffset')
-    print("startoffset:", startoffset)
+    f.write("startoffset:", startoffset)
     endoffset = issue.get('textRange').get('endOffset')  
-    print("endoffset:", endoffset)
+    f.write("endoffset:", endoffset)
+    f.write("```")
+    f.write("File Contents")
+    f.write("```")
+    filename = issue.get('component').split(":")[1]
     with open(filename, 'r') as file:
         content = file.read()
-        print(content)
-    #print(f"Issue Key: {issue.get('key')}")
-    #print(f"Severity: {issue.get('severity')}")
-    #print(f"Message: {issue.get('message')}")
-    #print(f"Component: {issue.get('component')}")
-    #print(f"Project: {issue.get('project')}")
-    #print("------")
+        f.write(content)
+    f.write("```")
+    f.close()
+
+    with open(prompt.txt, 'r') as file:
+        content = file.read()
+        f.write(content)
+    
+    break
