05822524f797c9163dfc39cc09925ee0ebd5ba69
Santosh Pawar <santoshjpawar@gmail.com>
2025-03-13 17:01:29 -0400

    Create get-scan-results.py

---
+++ .github/workflows/scripts/get-scan-results.py
@@ -0,0 +1,14 @@
+import requests
+from requests.auth import HTTPBasicAuth
+
+username = sys.argv[0]
+password = ""
+headers = {
+    'Accept': 'application/json',
+}
+params = {
+    'organization': 'sp-org-1',
+    'projects': 'sp-org-1_repo-1',
+}
+response = requests.get('https://sonarcloud.io/api/issues/search', params=params, headers=headers, auth=HTTPBasicAuth(username, password))
+print(response.json())
