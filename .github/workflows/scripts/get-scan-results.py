c434c75bd0641cdb8d9459496f28b5ebf442fe4d
Santosh Pawar <santoshjpawar@gmail.com>
2025-03-13 18:50:21 -0400

    Update get-scan-results.py

--- .github/workflows/scripts/get-scan-results.py
+++ .github/workflows/scripts/get-scan-results.py
@@ -18,55 +18,55 @@ def get_scan_results(sonar_token):
     headers = {
         'Accept': 'application/json',
     }
-      params = {
-          'organization': 'sp-org-1',
-          'projects': 'sp-org-1_repo-1',
-      }
-      response = requests.get('https://sonarcloud.io/api/issues/search', params=params, headers=headers, auth=HTTPBasicAuth(username, password))
+    params = {
+        'organization': 'sp-org-1',
+        'projects': 'sp-org-1_repo-1',
+    }
+    response = requests.get('https://sonarcloud.io/api/issues/search', params=params, headers=headers, auth=HTTPBasicAuth(username, password))
+    
+    # Parse the JSON response
+    jsonout = json.loads(response.text)
+    issues = jsonout.get('issues', [])
+    for issue in issues:
+        if issue.get('status') == "CLOSED":
+            continue
+        f = open("prompt.txt", "a")
+        with open(".github/workflows/scripts/action-prompt.txt", 'r') as file:
+            content = file.read()
+            f.write(content + "\n")
+        f.write("Vulnerability Details:\n")
+        f.write("```\n")
+        key = issue.get('key')
+        f.write("key:" + key + "\n")
+        rule = issue.get('rule')
+        f.write("rule:" + rule + "\n")
+        message = issue.get('message')
+        f.write("message:" + message + "\n")
+        if(issue.get('textRange') == None):
+            continue
+        startline = issue.get('textRange').get('startLine')
+        f.write("startline:" + str(startline) + "\n")
+        endline = issue.get('textRange').get('endLine')  
+        f.write("endline:" + str(endline) + "\n")
+        startoffset = issue.get('textRange').get('startOffset')
+        f.write("startoffset:" + str(startoffset) + "\n")
+        endoffset = issue.get('textRange').get('endOffset')  
+        f.write("endoffset:" + str(endoffset) + "\n")
+        f.write("```\n")
+        f.write("File Contents:" + "\n")
+        f.write("```\n")
+        filename = issue.get('component').split(":")[1]
+        with open(filename, 'r') as file:
+            content = file.read()
+            f.write(content + "\n")
+        f.write("```\n")
+        f.close()
+    
+        with open("prompt.txt", 'r') as file:
+            content = file.read()
+            print(content)
         
-      # Parse the JSON response
-      jsonout = json.loads(response.text)
-      issues = jsonout.get('issues', [])
-      for issue in issues:
-          if issue.get('status') == "CLOSED":
-              continue
-          f = open("prompt.txt", "a")
-          with open(".github/workflows/scripts/action-prompt.txt", 'r') as file:
-              content = file.read()
-              f.write(content + "\n")
-          f.write("Vulnerability Details:\n")
-          f.write("```\n")
-          key = issue.get('key')
-          f.write("key:" + key + "\n")
-          rule = issue.get('rule')
-          f.write("rule:" + rule + "\n")
-          message = issue.get('message')
-          f.write("message:" + message + "\n")
-          if(issue.get('textRange') == None):
-              continue
-          startline = issue.get('textRange').get('startLine')
-          f.write("startline:" + str(startline) + "\n")
-          endline = issue.get('textRange').get('endLine')  
-          f.write("endline:" + str(endline) + "\n")
-          startoffset = issue.get('textRange').get('startOffset')
-          f.write("startoffset:" + str(startoffset) + "\n")
-          endoffset = issue.get('textRange').get('endOffset')  
-          f.write("endoffset:" + str(endoffset) + "\n")
-          f.write("```\n")
-          f.write("File Contents:" + "\n")
-          f.write("```\n")
-          filename = issue.get('component').split(":")[1]
-          with open(filename, 'r') as file:
-              content = file.read()
-              f.write(content + "\n")
-          f.write("```\n")
-          f.close()
-        
-          with open("prompt.txt", 'r') as file:
-              content = file.read()
-              print(content)
-            
-          break
+        break
 
 # Execution starts here
 get_scan_results(sys.argv[1])
